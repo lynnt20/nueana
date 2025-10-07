9f08fb7af10e3dfb4ccbfc657cfb5dacf6cc74d5
lynnt20 <lynnt@uchicago.edu>
2025-10-07 14:44:39 -0500

    adding missing functions in init script

--- __init__.py
+++ __init__.py
@@ -9,10 +9,10 @@ from . import masks
 
 # Import commonly used functions for convenience
 from .constants import signal_dict, signal_labels, pdg_dict, colors
-from .utils import flatten_df, get_slc, get_evt, get_signal_evt, get_backgr_evt
+from .utils import flatten_df, get_slc, get_slices, get_evt, get_signal_evt, get_backgr_evt
 from .selections import cutPreselection, cutShowerEnergy, cutContainment, cutCRUMBS
 from .corrections import shw_energy_fix
-from .plotting import plot_var, plot_var_pdg, plot_mc_data
+from .plotting import plot_var, plot_var_pdg, plot_mc_data, data_plot_overlay
 from .masks import maskTrueVtxFv
 # from .dataframes import defineBackground, getPDGCounts, getPFP
 
