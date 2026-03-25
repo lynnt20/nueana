86a2f730bd66c04ed2ba395a3973aa66b8471f2c
lynnt20 <lynnt@uchicago.edu>
2026-03-25 14:24:39 -0500

    adding new constants to obtain xsec units, rescale integrated flux based on AV front face

--- constants.py
+++ constants.py
@@ -38,9 +38,6 @@ pdg_dict = {
     r"$\gamma$":   {"pdg":22,   },
     r"$p$":        {"pdg":2212, },
     r"$\pi^{+/-}$":{"pdg":211,  },
-    # "pi0": {"pdg":111, "mass":0.134976},
-    # "n": {"pdg":2112, "mass":0.939565},
-    # "other": {"pdg":0, "mass":0}
 }
 
 # flux file, units: /m^2/10^6 POT, 50 MeV bins
@@ -49,3 +46,12 @@ with uproot.open(fluxfile) as f:
     nue_flux = f["flux_sbnd_nue"].to_numpy()
     flux_vals = nue_flux[0]
 integrated_flux = flux_vals.sum()/1e4 # to cm2
+integrated_flux *= (180*180)/(200*200) # rescale the front face to AV front face
+
+RHO = 1.3836  #g/cm3, liquid Ar density
+N_A = 6.02214076e23 # Avogadro’s number
+M_AR = 40 # g, molar mass of argon
+# V_SBND = 380 * 380 * 440 # cm3, the active volume of the detector 
+# x cm (drift) * z cm (width) * y cm (height), excluding 90 cm of y-dimension at high z
+V_SBND = (190)*2 * ((250 - 10)*(190*2) + (450-250)*(100 + 190))
+NTARGETS = RHO * V_SBND * N_A / M_AR
