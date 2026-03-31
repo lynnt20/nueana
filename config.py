b5e672e70871a907544e067eabe973dcc23c034c
lynnt20 <lynnt@uchicago.edu>
2026-03-31 12:43:02 -0500

    add intime file and cleaned up detvar dicts to config, including signal/control region specific dicts

--- config.py
+++ config.py
@@ -23,13 +23,17 @@ if CAFPYANA_PATH not in sys.path:
 # Flux file path
 FLUX_FILE = "/exp/sbnd/data/users/lynnt/xsection/flux/sbnd_original_flux.root"
 
+# In-time cosmic sample file path
+INTIME_FILE = "/scratch/7DayLifetime/lynnt/MCP2025B_v10_06_00_09/intime.df"
+
 # Detector variation (detvar) dictionaries path
 # List of pickle files to load and combine for detector variations
 DETVAR_DICT_DIR = "/exp/sbnd/data/users/lynnt/xsection/samples/MCP2025B_v10_06_00_09/mc/dfs/detvars"
 DETVAR_DICT_FILES = [
-    "/exp/sbnd/data/users/lynnt/xsection/samples/MCP2025B_v10_06_00_09/mc/dfs/detvars/detvar_dict_updated.pkl",
-    "/exp/sbnd/data/users/lynnt/xsection/samples/MCP2025B_v10_06_00_09/mc/dfs/detvars/recomb_dict_updated2.pkl",
+    DETVAR_DICT_DIR + "/detvar_dict_combined.pkl",
 ]
+DETVAR_DICT_SIGNAL = DETVAR_DICT_DIR + "/detvar_dict_signal.pkl"
+DETVAR_DICT_CONTROL = DETVAR_DICT_DIR + "/detvar_dict_control.pkl"
 
 # Data directories
 DATA_DIR = "/exp/sbnd/data/users/lynnt/xsection/data"
@@ -52,5 +56,6 @@ VERIFY_PATHS = False
 if VERIFY_PATHS:
     _verify_path(CAFPYANA_PATH, "CAFPYANA_PATH")
     _verify_path(FLUX_FILE, "FLUX_FILE")
+    _verify_path(INTIME_FILE, "INTIME_FILE")
     for i, detvar_file in enumerate(DETVAR_DICT_FILES):
         _verify_path(detvar_file, f"DETVAR_DICT_FILES[{i}]")
